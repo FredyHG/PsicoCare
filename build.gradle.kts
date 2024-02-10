f0947e9385895e0cb5307de4fe210cbd3b5c5ee3
unknown <fredyultima2@gmail.com>
2024-02-10 17:36:24 -0300

    fix: disable sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 	id("org.springframework.boot") version "3.1.5"
 	id("io.spring.dependency-management") version "1.1.3"
 	id("application")
-	id("org.sonarqube") version "4.4.1.3373"
+//	id("org.sonarqube") version "4.4.1.3373"
 	id("jacoco")
 }
 
@@ -83,13 +83,13 @@ file("local.properties").inputStream().use {
 val sonarKey = properties.getProperty("SONAR_KEY")
 
 
-sonarqube {
-	properties {
-		property("sonar.projectKey", "psicocare")
-		property("sonar.host.url", "http://localhost:9000")
-		property("sonar.login", sonarKey)
-		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
-		property("sonar.exclusions", "**/models/**")
-		property("sonar.coverage.exclusions", "**/exceptions/**, **/dtos/**,PsicoCareApplication.java, **/controllers/**, **/security/configs/**")
-	}
-}
+//sonarqube {
+//	properties {
+//		property("sonar.projectKey", "psicocare")
+//		property("sonar.host.url", "http://localhost:9000")
+//		property("sonar.login", sonarKey)
+//		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+//		property("sonar.exclusions", "**/models/**")
+//		property("sonar.coverage.exclusions", "**/exceptions/**, **/dtos/**,PsicoCareApplication.java, **/controllers/**, **/security/configs/**")
+//	}
+//}
--- src/main/java/com/fredyhg/psicocare/models/dtos/patient/PatientPutRequest.java
+++ src/main/java/com/fredyhg/psicocare/models/dtos/patient/PatientPutRequest.java
@@ -1,18 +1,22 @@
 package com.fredyhg.psicocare.models.dtos.patient;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PatientPutRequest {
-    private Optional<String> name = Optional.empty();
-    private Optional<String> lastName = Optional.empty();
-    private Optional<LocalDate> birthDate = Optional.empty();
+    private Optional<String> name;
+    private Optional<String> lastName;
+    private Optional<LocalDate> birthDate;
     private String cpf;
-    private Optional<String> email = Optional.empty();
-    private Optional<String> phone = Optional.empty();
+    private Optional<String> email;
+    private Optional<String> phone;
 }
