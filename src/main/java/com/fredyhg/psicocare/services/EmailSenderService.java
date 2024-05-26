dd174b69a7c7c1c6444173a9b1b4dc8a4f529ef2
Fredy <fredyultima2@gmail.com>
2024-05-25 22:32:35 -0300

    feat: create email sender service interface to use solid practices

--- src/main/java/com/fredyhg/psicocare/services/EmailSenderService.java
+++ src/main/java/com/fredyhg/psicocare/services/EmailSenderService.java
@@ -1,131 +1,20 @@
 package com.fredyhg.psicocare.services;
 
-import com.fredyhg.psicocare.exceptions.utils.ParseEmailInfosException;
 import com.fredyhg.psicocare.models.PsychologistModel;
 import com.fredyhg.psicocare.models.TherapyModel;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StreamUtils;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.time.format.DateTimeFormatter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class EmailSenderService {
+public interface EmailSenderService {
 
-    private final JavaMailSender mailSender;
+    void sendEmail(TherapyModel therapyModel);
 
-    @Value("${spring.mail.from-username}")
-    private String emailFrom;
+    void sendAccessDetailsEmail(PsychologistModel psychologistModel, String password);
 
-    public void sendEmail(TherapyModel therapyModel) {
-        MimeMessage message = mailSender.createMimeMessage();
+    void schedulesDates(PsychologistModel psychologistModel, List<TherapyModel> sessions);
 
-        String dateFormatted = extractDataFromLocalDateTime(therapyModel.getDateTime());
-        String hourFormatted = extractHourFromLocalDateTime(therapyModel.getDateTime());
+    String extractDataFromLocalDateTime(LocalDateTime localDateTime);
 
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(therapyModel.getPatient().getEmail());
-            helper.setSubject("Appointment Confirmed");
-
-            ClassPathResource resource = new ClassPathResource("static/email-template.html");
-            String emailContent = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
-
-            emailContent = emailContent.replace("{patientName}", therapyModel.getPatient().getName());
-            emailContent = emailContent.replace("{psychologistName}", therapyModel.getPsychologist().getName());
-            emailContent = emailContent.replace("{dateFormatted}", dateFormatted);
-            emailContent = emailContent.replace("{hourFormatted}", hourFormatted);
-
-            helper.setText(emailContent, true);
-            helper.setFrom("PsicoCare <" + emailFrom + ">");
-        } catch (IOException | MessagingException ex) {
-            throw new ParseEmailInfosException("Error to parse email infos");
-        }
-
-        mailSender.send(message);
-    }
-
-    public void sendAccessDetailsEmail(PsychologistModel psychologistModel, String password) {
-        MimeMessage message = mailSender.createMimeMessage();
-
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(psychologistModel.getEmail());
-            helper.setSubject("Access Details");
-
-            ClassPathResource resource = new ClassPathResource("static/access-details-template.html");
-            String emailContent = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
-
-            emailContent = emailContent.replace("{psychologistName}", psychologistModel.getName());
-            emailContent = emailContent.replace("{username}", psychologistModel.getEmail());
-            emailContent = emailContent.replace("{password}", password);
-
-            helper.setText(emailContent, true);
-            helper.setFrom("PsicoCare <" + emailFrom + ">");
-        } catch (IOException | MessagingException ex) {
-            throw new ParseEmailInfosException("Error to parse email infos");
-        }
-
-        mailSender.send(message);
-    }
-
-    public void schedulesDates(PsychologistModel psychologistModel, List<TherapyModel> sessions){
-
-        StringBuilder therapySessionsHtml = new StringBuilder();
-
-        for(TherapyModel session : sessions){
-            therapySessionsHtml.append("<tr>")
-                    .append("<td>").append(extractHourFromLocalDateTime(session.getDateTime())).append("</td>")
-                    .append("<td>").append(session.getPatient().getName()).append("</td>")
-                    .append("</tr>");
-        }
-
-
-        MimeMessage message = mailSender.createMimeMessage();
-
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(psychologistModel.getEmail());
-            helper.setSubject("Today Schedules");
-
-            ClassPathResource resource = new ClassPathResource("static/schedules-template.html");
-            String emailContent = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
-
-            emailContent = emailContent.replace("${therapySessions}", therapySessionsHtml.toString());
-
-            helper.setText(emailContent, true);
-            helper.setFrom("PsicoCare <" + emailFrom + ">");
-        } catch (IOException | MessagingException ex) {
-            throw new ParseEmailInfosException("Error to parse email infos");
-        }
-
-        mailSender.send(message);
-    }
-
-
-
-    public String extractDataFromLocalDateTime(LocalDateTime localDateTime){
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        return localDateTime.format(format);
-    }
-
-    public String extractHourFromLocalDateTime(LocalDateTime localDateTime){
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm");
-
-        return localDateTime.format(format);
-    }
-
+    String extractHourFromLocalDateTime(LocalDateTime localDateTime);
 }
