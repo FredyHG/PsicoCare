1a44bf310fe44df3e8b0266ebed7c423f82d9857
Fredy <fredyultima2@gmail.com>
2024-05-25 22:47:22 -0300

    feat: create confirmation code service interface to use solid practices

--- src/main/java/com/fredyhg/psicocare/services/ConfirmationCodeService.java
+++ src/main/java/com/fredyhg/psicocare/services/ConfirmationCodeService.java
@@ -1,42 +1,12 @@
 package com.fredyhg.psicocare.services;
 
-import com.fredyhg.psicocare.exceptions.code.ConfirmationCodeNotFoundException;
 import com.fredyhg.psicocare.models.ConfirmationCodeModel;
 import com.fredyhg.psicocare.models.TherapyModel;
-import com.fredyhg.psicocare.repositories.ConfirmationCodeRepository;
-import com.fredyhg.psicocare.utils.Utils;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class ConfirmationCodeService {
+public interface ConfirmationCodeService {
+    ConfirmationCodeModel getByTherapy(TherapyModel therapyModel);
 
-    private final ConfirmationCodeRepository confirmationCodeRepository;
+    void createConfirmationCodeModel(TherapyModel therapyModel);
 
-    public ConfirmationCodeModel getByTherapy(TherapyModel therapyModel){
-        return confirmationCodeRepository.findByTherapy(therapyModel).orElseThrow(
-                () -> new ConfirmationCodeNotFoundException("Confirmation code not found")
-        );
-    }
-
-    public void createConfirmationCodeModel(TherapyModel therapyModel){
-        ConfirmationCodeModel confirmationCode = ConfirmationCodeModel.builder()
-                .used(false)
-                .therapy(therapyModel)
-                .code(String.valueOf(Utils.genConfirmCode()))
-                .build();
-
-        confirmationCodeRepository.save(confirmationCode);
-    }
-
-    public void toggleUse(ConfirmationCodeModel confirmationCodeModel){
-
-        ConfirmationCodeModel confirmationCode = confirmationCodeRepository.findById(confirmationCodeModel.getId()).orElseThrow(
-                () -> new ConfirmationCodeNotFoundException("Confirmation code not found"));
-
-        confirmationCode.setUsed(true);
-
-        confirmationCodeRepository.save(confirmationCode);
-    }
+    void toggleUse(ConfirmationCodeModel confirmationCodeModel);
 }
